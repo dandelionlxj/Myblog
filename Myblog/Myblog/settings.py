05c77203ca4a94c0cd1209cda811ac055c657c74
dandelionlxj <1611462783@qq.com>
2019-10-26 22:06:59 +0800

    update

--- Myblog/Myblog/settings.py
+++ Myblog/Myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rx5dlei_uf(4jesleue(@d5p55(n=)_by(bviqa^h9#20vl23&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.zmrenwu.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'ststic')
